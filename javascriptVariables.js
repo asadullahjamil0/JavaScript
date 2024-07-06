fb794156da7dc24527b5a69fcf8c894b0abb746f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-06 21:18:59 +0500

    JavaScript Variables

---
+++ javascriptVariables.js
@@ -0,0 +1,10 @@
+var myName = "Asadullah";
+myName = "Abdullah jamil";
+myName = "Asadullah jamil";
+var yourName = prompt("What is your name?");
+alert("Hello my name is " + myName + " Welcome to my channel " + yourName);
+
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert("Your gameLevel is: " + gameLevel);
