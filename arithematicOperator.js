b2889697c91f4bf25ad8cec16fb47bf5dfb7868f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-13 00:40:15 +0500

    minor change

--- arithematicOperator.js
+++ arithematicOperator.js
@@ -1,3 +1,3 @@
 var dogAge = prompt("What's your dog's age: ");
 var humanAge = (dogAge - 2) * 4 + 21;
-alert("Your dog's age as a human age would be: " + humanAge);
+alert("Your dog is " + humanAge + " years old in human years.");
