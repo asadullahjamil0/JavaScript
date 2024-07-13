b6544751e60e126c206606d0cc47e2aa9bab328e
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-13 20:26:33 +0500

    Increment and decrement in js.

---
+++ increDecree.js
@@ -0,0 +1,10 @@
+
+// for decrement
+for (let x = 12; x > 0; x -= 2) {
+    alert("this is number: " + x);
+}
+
+// for increment
+for (let x = 0; x < 6; x++) {
+    alert("this is number: " + x);
+}
