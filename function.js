7e60709663da0f9bf3bba148f44d9712f14ea310
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-13 20:45:55 +0500

    How to create a function in javaScript.

---
+++ function.js
@@ -0,0 +1,23 @@
+function getMilk(){
+    alert("leaveHouse");
+    alert("moveRight");
+    alert("moveRight");
+    alert("moveUp");
+    alert("moveUp");
+    alert("moveUp");
+    alert("moveUp");
+    alert("moveRight");
+    alert("moveRight");
+    alert("buyMilk");
+    alert("moveLeft");
+    alert("moveLeft");
+    alert("moveDown");
+    alert("moveDown");
+    alert("moveDown");
+    alert("moveDown");
+    alert("moveRight");
+    alert("moveRight");
+    alert("enterHouse");
+    
+}
+getMilk();
