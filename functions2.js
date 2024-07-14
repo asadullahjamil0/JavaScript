c0c1337beb74a00016f8a23053c4a79f0666ba43
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-14 22:07:20 +0500

    Functions:Arguements and parameters.

---
+++ functions2.js
@@ -0,0 +1,26 @@
+function getMilk(cost){
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    var numberOfBottles = (cost / 1.5);
+    console.log("Buy " + Math.floor(numberOfBottles) + " bottles of milk");
+    
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("enterHouse");
+    
+}
+getMilk(7);
