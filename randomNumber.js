1150bbd2647eef0f93eb24dc788d2f690d800b08
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-16 22:58:36 +0500

    Random Number generator in js.

---
+++ randomNumber.js
@@ -0,0 +1,7 @@
+
+prompt("What is your name?");
+prompt("What is a name of your crush?");
+
+var loveScore = Math.random();
+loveScore = Math.floor((loveScore*100) + 1);
+alert("Your love Score is "+ loveScore + "%");
