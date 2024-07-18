f02f47c5a841902b60aba5f25e00e2a529c56cc5
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-18 20:49:34 +0500

    Working with Javascript Arrays.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var guestList = ["asad","izhaar","intzaar","qraar","sakoon"];
+var guestName = prompt("What is your name?");
+if(guestList.includes(guestName)){
+    alert("welcome to the party!");
+}
+else{
+    alert("Sorry your are not welcomed here!");
+}
