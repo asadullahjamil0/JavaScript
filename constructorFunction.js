3f35b45696c1e3e4534ce398caffebe1d29cb310
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-06 17:05:30 +0500

    Added function in constructor function in js

--- constructorFunction.js
+++ constructorFunction.js
@@ -1,9 +1,12 @@
-function HouseKeeper(name, age, workExperience, cleaningReportoire) {
+function HouseKeeper (name,age,workExperience,cleaningReportoire){
     this.name = name;
     this.age = age;
     this.workExperience = workExperience;
     this.cleaningReportoire = cleaningReportoire;
+    this.cleanHouse = function(){
+        alert("Cleaning house progress.Please wait for some time")
+    }
 }
 
-var houseKeeper1 = new HouseKeeper("Angela", 24, 6, ["Bathroom", "bed", "drawing room"])
-console.log("My name is " + houseKeeper1.name + " and my age is " + houseKeeper1.age);
+var houseKeeper1 = new HouseKeeper("Angela",24,6,["Bathroom","bed","drawing room"])
+houseKeeper1.cleanHouse();
