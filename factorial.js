e599c5a1c60d4c7ea2754381ab16678d561c2ac7
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-08 11:40:32 +0500

    minor change

--- factorial.js
+++ factorial.js
@@ -17,11 +17,10 @@ console.log(arr1.reduce(red))
 let a = 6
 function factorial(number) {
     let arr = Array.from(Array(number + 1).keys())
-    console.log(arr.slice(1,))
     let c = arr.slice(1,).reduce((a, b) => {
         return a * b
     })
-    console.log(c)
+    return c;
 }
 
 factorial(a)
