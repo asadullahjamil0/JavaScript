150a14ba94e2d55f6d80953a8b742ff451065f9e
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-08 11:39:26 +0500

    Another method to calculate factorial in js

--- factorial.js
+++ factorial.js
@@ -1,7 +1,7 @@
-a = prompt("Please enter the number for factorial: ")
+num1 = prompt("Please enter the number for factorial: ")
 let arr1 = [];
 
-for (let index = 1; index <= a; index++) {
+for (let index = 1; index <= num1; index++) {
     arr1.push(index);
 
 }
@@ -11,3 +11,17 @@ const red = (a, b) => {
 }
 
 console.log(arr1.reduce(red))
+
+
+// Anohter method to calculate the factorial
+let a = 6
+function factorial(number) {
+    let arr = Array.from(Array(number + 1).keys())
+    console.log(arr.slice(1,))
+    let c = arr.slice(1,).reduce((a, b) => {
+        return a * b
+    })
+    console.log(c)
+}
+
+factorial(a)
