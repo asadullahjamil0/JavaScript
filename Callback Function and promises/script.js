b69277aeb5ed823923c550844ed1cb9077e7f393
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-12 13:43:49 +0500

    Concept of callback hell in javaScript

--- Callback Function and promises/script.js
+++ Callback Function and promises/script.js
@@ -13,16 +13,40 @@
 
 
 
-const callBack = (arg) => {
-    console.log(arg)
+// const callBack = (arg) => {
+//     console.log(arg)
+// }
+
+
+// const loadScript = (src, callBack) => {
+//     let sc = document.createElement("script");
+//     sc.src = src;
+//     sc.onload = callBack("AsadUllah");
+//     document.head.append(sc);
+// }
+
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callBack)
+
+
+// Another example of callback
+
+function getData(dataId, getNextData) {
+    setTimeout(() => {
+        console.log("data", dataId)
+        if (getNextData) {
+            getNextData();
+        }
+    }, 2000);
 }
 
-
-const loadScript = (src, callBack) => {
-    let sc = document.createElement("script");
-    sc.src = src;
-    sc.onload = callBack("AsadUllah");
-    document.head.append(sc);
-}
-
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callBack)
+// We called this code callback hell because of to much difficult to understand
+getData(1, () => {
+    console.log("Getting data2.....")
+    getData(2, () => {
+        console.log("Getting data3.....")
+        getData(3, () => {
+            console.log("Getting data4.....")
+            getData(4);
+        })
+    });
+})
