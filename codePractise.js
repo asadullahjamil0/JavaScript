1c6e80e41637ca6e1ff37cf0e4229f0aff34baa4
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-13 19:51:47 +0500

    how to get file extension name

--- codePractise.js
+++ codePractise.js
@@ -1,3 +1,9 @@
-const isEqualTo = (a,b) => a === 100 || b === 100 || a + b ===100;
- console.log(isEqualTo(10,89));
- console.log(isEqualTo(10,100));
+// Exercise - 1
+// const isEqualTo = (a,b) => a === 100 || b === 100 || a + b ===100;
+//  console.log(isEqualTo(10,89));
+//  console.log(isEqualTo(10,100));
+
+
+// Exercise - 2
+const toGetExtension = (str) => str.slice(str.lastIndexOf("."));
+console.log(toGetExtension("script.js"));
