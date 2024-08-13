ccf1deb536e09e6f01608c22574cba0435934426
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-13 15:45:37 +0500

    Mini-project of currency exchanger with using Currency-exchange api

--- FetchApi/script.js
+++ FetchApi/script.js
@@ -1,12 +1,24 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
-let factPara = document.querySelector("#fact");
 
 
-const getFacts = async () => {
-    console.log("Getting data.....")
+// This method is by async-await with same result
+const api = async () => {
+    console.log("Getting data........");
     let response = await fetch(URL);
-    console.log(response)
+    console.log(response);
     let data = await response.json();
-    factPara.innerText = data[3].text;
+    document.querySelector("#fact").innerText = data[4].text;
 }
-let btnF = document.querySelector("#btn").addEventListener("click", getFacts);
+
+
+// This method is by promises with same result
+// function api() {
+//     fetch(URL).then((response) => {
+//         console.log(response)
+//         return response.json();
+//     }).then((data) => {
+//         document.querySelector("#fact").innerText = data[4].text;
+//     })
+// }
+
+document.querySelector("#btn").addEventListener("click", api);
