8e0854537d2049d2afec09de098e1caa320f26fc
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-14 19:51:55 +0500

    parseInt method

---
+++ tryCatchAndErrorHandling.js
@@ -0,0 +1,11 @@
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+
+let sum = parseInt(a) + parseInt(b)
+
+if (sum === 100 || a === 100 || b === 100) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
