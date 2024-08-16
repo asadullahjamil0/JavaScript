3d35a9cf1fb024507339982aa5c5ccb2cfb7b5f7
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-16 18:17:47 +0500

    Check if number is negative

--- Code Practise/square.js
+++ Code Practise/square.js
@@ -1,8 +1,26 @@
-let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 9]
+// To check if number is more than once
+// let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 9]
+// let double = []
+// let set = new Set();
+
+// for (const number of num) {
+//     if (!set.has(number)) {
+//         double.push(number * 2)
+//         set.add(number)
+//     }
+// }
+// console.log(double)
+
+
+// To check if number is negative in an array
+let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, -8, 9, 9]
 let double = []
 let set = new Set();
 
 for (const number of num) {
+    if (number < 0) {
+        break;
+    }
     if (!set.has(number)) {
         double.push(number * 2)
         set.add(number)
