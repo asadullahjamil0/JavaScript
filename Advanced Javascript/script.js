0547bda6679fd171558dc7d5a47188994358d161
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-16 16:58:42 +0500

    Desructure

--- Advanced Javascript/script.js
+++ Advanced Javascript/script.js
@@ -6,8 +6,22 @@ async function sleep() {
     })
 }
 (async function main() {
-    let a = await sleep();
-    console.log(a)
-    let b = await sleep();
-    console.log(b)
+    // let a = await sleep();
+    // console.log(a)
+    // let b = await sleep();
+    // console.log(b)
+
+
+    // destructure
+    // let [x, y, ...rest] = [4, 6, 3, 3, 4, 5, 6, 2]
+    // console.log(x, y, rest);
+
+    let obj = {
+        a: 1,
+        b: 2,
+        c: 3
+    }
+
+    let { a, b } = obj
+    console.log(a, b);
 })()
