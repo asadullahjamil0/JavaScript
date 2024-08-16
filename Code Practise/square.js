f80ab1f18c5ecf001d08eece881a2fd95f17ce32
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-16 18:30:08 +0500

    Check if the string contain any vowel and then count these vowels in array

--- Code Practise/square.js
+++ Code Practise/square.js
@@ -17,13 +17,26 @@ let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, -8, 9, 9]
 let double = []
 let set = new Set();
 
-for (const number of num) {
-    if (number < 0) {
-        break;
-    }
-    if (!set.has(number)) {
-        double.push(number * 2)
-        set.add(number)
+// for (const number of num) {
+//     if (number < 0) {
+//         break;
+//     }
+//     if (!set.has(number)) {
+//         double.push(number * 2)
+//         set.add(number)
+//     }
+// }
+// console.log(double)
+
+// Problem regarding vowel
+let str = "Hey how are you i am good";
+let spl = str.split('');
+let checkVowel = []
+let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+for (const splt of spl) {
+    if (vowels.includes(splt)) {
+        checkVowel.push(splt)
     }
 }
-console.log(double)
+console.log(checkVowel)
