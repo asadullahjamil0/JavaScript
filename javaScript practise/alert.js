1d13372bfd6dedc87aa262e26f3e2dd03b8fc3a7
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-22 17:52:53 +0500

    Some more types of functions in js

--- javaScript practise/alert.js
+++ javaScript practise/alert.js
@@ -36,3 +36,35 @@ function countVowel(str) {
     }
     console.log(count)
 }
+// Count vowels wtih arrow function
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    return count;
+}
+
+// for each function
+let arr = ["Lahore", "Karachi", "Islamabad", "Sheikhupura", "Sialkot"]
+arr.forEach((val, idx, arr) => {
+    if (val === "Sheikhupura") {
+        console.log("Welcome to " + val.toUpperCase(), idx, arr);
+    }
+})
+
+// Map function
+let arr_1 = [4, 8, 3, 7, 6]
+let calSqr = arr_1.map((sqr) => {
+    return (sqr * sqr);
+})
+console.log(calSqr)
+
+//filter function
+let arr_2 = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+const calcMod = arr_2.filter((val) => {
+    return val % 2 === 0;
+})
+console.log(calcMod);
