e24cf1888d693498abe5b8cf101501f07e22fb07
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-24 23:51:32 +0500

    Practise for arrays

--- Arrays and methods/arrays.js
+++ Arrays and methods/arrays.js
@@ -19,3 +19,25 @@ for (let i = 0; i < fictionalCharacter.length; i++) {
     console.log("item no: " + (i + 1) + " " + fictionalCharacter[i])
 }
 
+let layMen = ["Charles", "John", "mark"]
+layMen.unshift("David")
+layMen.shift();
+const concatArray = fictionalCharacter.concat(layMen);
+console.log(concatArray);
+
+// Slice and splice method
+// slice method is used to get specific part of an array
+console.log(marks.slice(1, 3));
+
+// splice method is used to change the original array
+marks.splice(2, 3, 77, 54, 69);
+console.log(marks);
+
+// Its a practise question at the end of the topic
+let companyName = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+companyName.shift();
+console.log(companyName);
+companyName.splice(1, 1, "Ola");
+console.log(companyName);
+companyName.push("Amazon");
+console.log(companyName);
