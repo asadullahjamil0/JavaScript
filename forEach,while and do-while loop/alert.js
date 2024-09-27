b1c3dfe239764cafe28c1e9ce53d20aee06e4e60
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-27 19:28:40 +0500

    For loop

---
+++ forEach,while and do-while loop/alert.js
@@ -0,0 +1,7 @@
+// For loop to iterate in array
+
+let cities = ["Lahore", "Karachi", "Islamabad", "Sialkot", "Faislabad"];
+for (let index = 0; index < cities.length; index++) {
+    console.log(cities[index]);
+
+}
