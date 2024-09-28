1ddf064dff80c2b778640926261566059ce0c22f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-09-28 22:39:48 +0500

    Map and its methods

---
+++ map,arrow function,conversion,classes/alert.js
@@ -0,0 +1,35 @@
+let myMap = new Map();
+
+myMap.set(1, "Lahore");
+myMap.set(2, "Karachi");
+myMap.set(3, "Islamabad");
+myMap.set(4, "Sialkot");
+
+// This is by for of loop
+// for just keys of map
+for (let key of myMap.keys()) {
+    console.log(`key is ${key}`)
+}
+// for just values
+for (let value of myMap.values()) {
+    console.log(`Value is ${value}`)
+}
+// for both values and keys
+for (let [key, value] of myMap) {
+    console.log(`key is ${key} and Value is ${value}`)
+}
+
+// by for each loop
+myMap.forEach((value, key) => {
+    console.log(`Value is ${value} and key is ${key}`)
+});
+
+// Map methods
+// to check size
+console.log(myMap.size)
+// to delete any key value
+myMap.delete(2)
+console.log(myMap);
+// we can also clear all the elements by using clear() function
+// myMap.clear();
+myMap.has("Lahore")
